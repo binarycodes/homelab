76a29343eaf77e68874d0b724da7dc697fef590d
Sujoy Das <sujoy.cse@gmail.com>
2024-05-12 21:06:03 +0300

    install iperf3 in all vms, set searchdomain, check ssh-keys with lower case hostnames

--- proxmox/terraform/debian_vm/main.tf
+++ proxmox/terraform/debian_vm/main.tf
@@ -38,8 +38,10 @@ resource "proxmox_vm_qemu" "debian" {
     }
   }
 
-  ipconfig0  = var.config.dhcp ? "ip=dhcp" : "ip=${var.config.ip_v4},gw=${var.config.gateway}"
-  nameserver = var.config.dhcp ? null : var.config.nameserver
+  ipconfig0    = var.config.dhcp ? "ip=dhcp" : "ip=${var.config.ip_v4},gw=${var.config.gateway}"
+  nameserver   = var.config.dhcp ? null : var.config.nameserver
+  searchdomain = var.config.searchdomain
+
   ciuser     = var.config.username
   cipassword = var.config.password
   sshkeys    = var.config.ssh_keys
