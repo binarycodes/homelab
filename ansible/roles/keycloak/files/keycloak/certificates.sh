413cf840189e0912c9e90e24f3fbdff4a892b3c0
Sujoy Das <me@binarycodes.io>
2025-03-22 21:32:17 +0200

    simplify keycloak/traefik setup
    no need to stop the docker compose, it checks for changes
    remove certificate generation for keycloak, using http behind traefik
    update keycloak, mariadb versions

--- ansible/roles/keycloak/files/keycloak/certificates.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout keycloak_server.key -out keycloak_server.crt -subj "/C=FI/ST=Turku/L=Turku/O=Vaadin/OU=Services/CN=localhost" -addext "subjectAltName = DNS:localhost,DNS:keycloak"
-openssl x509 -outform der -in keycloak_server.crt -out keycloak_server.der
-
-chmod 644 keycloak_server.key
