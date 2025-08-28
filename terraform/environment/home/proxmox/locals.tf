10a785c46ad30ec502f44439d08e413360de388d
Sujoy Das <me@binarycodes.io>
2025-08-29 01:12:06 +0300

    proxmox cloud images for debian trixie and home assistant

--- terraform/environment/home/proxmox/locals.tf
+++ terraform/environment/home/proxmox/locals.tf
@@ -1,3 +1,15 @@
 locals {
   nodes = ["pve1", "pve2", "pve3"]
+
+  debian_trixie = {
+    url            = "https://cloud.debian.org/images/cloud/trixie/20250814-2204/debian-13-generic-amd64-20250814-2204.qcow2"
+    checksum       = "8f5c54d654b53951430b404efc3043b425cf2214467d5bf33d6c5157fa47c8fe4a1a2abf603050dafc7e54f57e9685f0d59a6c0d09d0cb2b7fcec75561c0df6f"
+    save_file_name = "debian-13-generic-amd64-20250814-2204.qcow2.img"
+  }
+
+  home_assistant = {
+    url                     = "https://github.com/home-assistant/operating-system/releases/download/16.1/haos_generic-x86-64-16.1.img.xz"
+    decompression_algorithm = "zst"
+    save_file_name          = "haos_generic-x86-64-16.1.img"
+  }
 }
--- terraform/environment/home/proxmox/main.tf
+++ terraform/environment/home/proxmox/main.tf
@@ -1,11 +1,23 @@
-resource "proxmox_virtual_environment_download_file" "trixie_cloud_image" {
+resource "proxmox_virtual_environment_download_file" "debian_trixie_cloud_image" {
   content_type       = "iso"
   datastore_id       = "local"
-  file_name          = "debian-13-generic-amd64-20250814-2204.qcow2.img"
   node_name          = each.value
-  url                = "https://cloud.debian.org/images/cloud/trixie/20250814-2204/debian-13-generic-amd64-20250814-2204.qcow2"
-  checksum           = "8f5c54d654b53951430b404efc3043b425cf2214467d5bf33d6c5157fa47c8fe4a1a2abf603050dafc7e54f57e9685f0d59a6c0d09d0cb2b7fcec75561c0df6f"
+  file_name          = local.debian_trixie.save_file_name
+  url                = local.debian_trixie.url
+  checksum           = local.debian_trixie.checksum
   checksum_algorithm = "sha512"
+
+  for_each = toset(local.nodes)
+}
+
+resource "proxmox_virtual_environment_download_file" "home_assistant_cloud_image" {
+  content_type            = "iso"
+  datastore_id            = "local"
+  file_name               = local.home_assistant.save_file_name
+  node_name               = each.value
+  url                     = local.home_assistant.url
+  decompression_algorithm = local.home_assistant.decompression_algorithm
+  overwrite               = false # file size will always differ due to decompression
 
   for_each = toset(local.nodes)
 }
