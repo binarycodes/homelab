d160517ca92239ffa20b09e075598a473e1f98a6
Sujoy Das <me@binarycodes.io>
2025-08-30 21:56:18 +0300

    create admin vm to manage all required infra (outside kubernetes)

---
+++ terraform/environment/home/admin/main.tf
@@ -0,0 +1,19 @@
+data "local_file" "ssh_public_key" {
+  filename = "../../../files/id_homelab.pub"
+}
+
+module "proxmox_debian_trixie_kubernetes" {
+  source = "../../../modules/debian-trixie-template"
+
+  for_each = local.vms
+  config   = each.value
+
+  ssh_authorized_key = trimspace(data.local_file.ssh_public_key.content)
+}
+
+output "vm_ipv4_address" {
+  value = {
+    for k, m in module.proxmox_debian_trixie_kubernetes :
+    k => m.vm_ipv4_address
+  }
+}
