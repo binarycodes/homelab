c06e101007808d728f51d719061c07cc7aa35edc
Sujoy Das <me@binarycodes.io>
2025-09-24 20:02:41 +0300

    add SDN.Allocate role to manage SDN from terraform

--- ansible/roles/proxmox_cluster/files/terraform-user.sh
+++ ansible/roles/proxmox_cluster/files/terraform-user.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pveum role add TerraformProv -privs "Datastore.Allocate Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt SDN.Use"
+pveum role add TerraformProv -privs "Datastore.Allocate Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt SDN.Use SDN.Allocate"
 
 pveum user add tfuser@pve
 pveum aclmod / -user tfuser@pve -role TerraformProv
