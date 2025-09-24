9878e7c75088d55209af51e2b98e3b938611d6be
Sujoy Das <me@binarycodes.io>
2025-09-24 19:17:40 +0300

    update proxmox provider version

--- terraform/modules/debian-trixie-template/versions.tf
+++ terraform/modules/debian-trixie-template/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.83.1"
+      version = "0.84.0"
     }
     keycloak = {
       source  = "keycloak/keycloak"
