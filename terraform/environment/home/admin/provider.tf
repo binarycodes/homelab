ee04ba7b3a73c1b3b7377e5920d59e1bb31c72d8
Sujoy Das <me@binarycodes.io>
2025-10-25 22:49:45 +0300

    update proxmox provider version

--- terraform/environment/home/admin/provider.tf
+++ terraform/environment/home/admin/provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.84.0"
+      version = "0.85.1"
     }
     keycloak = {
       source  = "keycloak/keycloak"
