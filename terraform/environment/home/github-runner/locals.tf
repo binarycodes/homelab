8d19136a416fbe4be6a1b7a14bc67756bef8c14b
Sujoy Das <me@binarycodes.io>
2025-12-07 02:33:44 +0200

    github self hosted runner vm (ephemeral docker runners)

---
+++ terraform/environment/home/github-runner/locals.tf
@@ -0,0 +1,27 @@
+locals {
+  secret = data.infisical_secrets.app.secrets
+
+  vm_config = {
+    pve3 = [
+      {
+        name   = "runner"
+        dhcp   = true
+        bridge = "LabNet"
+        tags   = ["runner"]
+      },
+    ]
+  }
+
+  vms = merge(
+    [for key, val in local.vm_config : {
+      for conf in val :
+      conf.name => merge(conf, {
+        node         = key,
+        username     = local.secret.vm_username.value,
+        user_id      = local.secret.vm_user_id.value,
+        timezone     = local.secret.vm_timezone.value,
+        searchdomain = local.secret.dns_zone.value,
+        age_secret   = local.secret.age_secret.value,
+      }) }
+  ]...)
+}
