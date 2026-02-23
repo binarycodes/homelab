65458c7471e06159b3c0200ef75039ced8c7a804
Sujoy Das <me@binarycodes.io>
2026-02-23 14:54:55 +0200

    create edge node to handle incomming requests

---
+++ terraform/environment/home/edge/locals.tf
@@ -0,0 +1,32 @@
+locals {
+  secret     = data.infisical_secrets.app.secrets
+  image_name = "debian-container-20260223-1050.qcow2.img"
+
+  vm_config = {
+    pve2 = [
+      {
+        name             = "edge"
+        dhcp             = false
+        ip4_address_cidr = "10.88.0.210/24"
+        gateway          = "10.88.0.1"
+        dns_addresses    = ["10.88.0.1"]
+        bridge           = "vmbr0"
+        tags             = ["edge"]
+        disk_size        = 32
+      },
+    ]
+  }
+
+  vms = merge(
+    [for key, val in local.vm_config : {
+      for conf in val :
+      conf.name => merge(conf, {
+        node         = key,
+        username     = local.secret.vm_username.value,
+        user_id      = local.secret.vm_user_id.value,
+        timezone     = local.secret.vm_timezone.value,
+        searchdomain = local.secret.dns_zone.value,
+        age_secret   = local.secret.age_secret.value,
+      }) }
+  ]...)
+}
---
+++ terraform/environment/home/edge/main.tf
@@ -0,0 +1,24 @@
+data "infisical_secrets" "app" {
+  workspace_id = var.infisical_project_id
+  env_slug     = var.infisical_environment
+  folder_path  = "/terraform"
+}
+
+module "proxmox_debian_trixie" {
+  source = "../../../modules/custom-debian-trixie-template"
+
+  for_each               = local.vms
+  config                 = each.value
+  image_name             = local.image_name
+  ca_keycloak_realm      = local.secret.ca_keycloak_realm.value
+  ca_keycloak_server_url = local.secret.ca_keycloak_server_url.value
+  ca_keycloak_token_url  = local.secret.ca_keycloak_token_url.value
+  ca_user_public_key     = local.secret.ca_user_public_key.value
+}
+
+output "vm_ipv4_address" {
+  value = {
+    for k, m in module.proxmox_debian_trixie :
+    k => m.vm_ipv4_address
+  }
+}
