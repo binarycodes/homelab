9be66e48155dfd7ba09a76d3525c7c8de2f09152
Sujoy Das <me@binarycodes.io>
2026-06-22 17:02:47 +0300

    chore: version updates

--- terraform/environment/home/github-runner/locals.tf
+++ terraform/environment/home/github-runner/locals.tf
@@ -1,6 +1,6 @@
 locals {
   secret     = data.infisical_secrets.app.secrets
-  image_name = "debian-container-20260613-1023.qcow2.img"
+  image_name = "debian-container-20260622-1042.qcow2.img"
 
   vm_config = {
     pve3 = [
--- terraform/environment/home/proxmox/locals.tf
+++ terraform/environment/home/proxmox/locals.tf
@@ -16,18 +16,18 @@ locals {
 
   cloud_images = {
     home_assistant = {
-      download_url            = "https://github.com/home-assistant/operating-system/releases/download/17.3/haos_ova-17.3.qcow2.xz"
-      save_file_name          = "haos_ova-17.3.qcow2.img"
-      checksum                = "d42fadf806c0690792a4460ff3a72c2846c4e16e2033aefad0835662e7a9696f"
+      download_url            = "https://github.com/home-assistant/operating-system/releases/download/18.0/haos_ova-18.0.qcow2.xz"
+      save_file_name          = "haos_ova-18.0.qcow2.img"
+      checksum                = "ae125c592532a4b64a5c05c6596173eb5edddf15a1e681e082ab28b7d5e5ba6b"
       checksum_algorithm      = "sha256"
       decompression_algorithm = "zst"
       overwrite               = false
     }
 
     free_bsd = {
-      download_url            = "https://download.freebsd.org/releases/VM-IMAGES/15.0-RELEASE/amd64/Latest/FreeBSD-15.0-RELEASE-amd64-BASIC-CLOUDINIT-ufs.qcow2.xz"
-      save_file_name          = "FreeBSD-15.0-RELEASE-amd64-BASIC-CLOUDINIT-ufs.qcow2.img"
-      checksum                = "ef2835411accb622f42dad145e5cdd91b703dfa972d33cce4d4b71c88b25f5892eb52e11826e78629dd42835b57111f1849d993ed08c6fd578b1070e4ed62379"
+      download_url            = "https://download.freebsd.org/releases/VM-IMAGES/15.1-RELEASE/amd64/Latest/FreeBSD-15.1-RELEASE-amd64-BASIC-CLOUDINIT-ufs.qcow2.xz"
+      save_file_name          = "FreeBSD-15.1-RELEASE-amd64-BASIC-CLOUDINIT-ufs.qcow2.img"
+      checksum                = "bea82b91a983a20eb7ecdc6f11c1006d394ebbc5668b7f0ecdecf5a54fc5f9ea3a8384fc9f74a6611a9b08c183fa953327147ebfaa0de199fd015e67dd1e608a"
       checksum_algorithm      = "sha512"
       decompression_algorithm = "zst"
       overwrite               = false
